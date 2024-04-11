e35feb2f7f6a2c4937e1a29077d110cc216cc726
김서준 <126214700+selffish234@users.noreply.github.com>
2024-04-11 20:28:02 +0200

    Add files via upload

---
+++ Solidity_tutorial/ERC-20/Burn.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Burn is ERC20, Ownable {
+    address adrs = address(0x9d149Aeed23eC26453037feF199127DFdF31ddE2);
+    constructor(uint _amount) ERC20("selffish Coin", "selffish") Ownable(adrs){
+        _mint(owner(), _amount * 10**18);
+        // owner == owner()
+    }
+
+    function burnCoin(uint _amount) public {
+        _burn(msg.sender, _amount * 10**18);
+    }
+}
